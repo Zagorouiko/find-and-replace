26007ac54b32b4cdc739ae61d5a6e7a39199f365
Leon Zagorouiko <Zagorouiko@gmail.com>
2015-05-29 14:17:13 -0700

    add spec for new function.

--- spec/specs.js
+++ spec/specs.js
@@ -2,8 +2,10 @@ describe("normalizePhrase", function() {
   it("input string is lowercased, split into an array, and punctuation is removed", function() {
     expect(normalizePhrase("HELLO,.,., world")).to.equal(["hello", "world"]);
   });
+});
 
   describe("findReplace", function() {
-    it("user inputs a sentence and ")
-  })
+    it("user inputs a string, a word to replace, and the replacement word, and it outputs the changed string", function() {
+      expect(findReplace("hello world")).to.equal("hello universe");
+    });
 });
