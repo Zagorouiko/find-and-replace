6350763853c4cd747e07f1148b8b7681804239a8
Leon Zagorouiko <student@epicodus.com>
2015-05-29 10:09:15 -0700

    add jquery for the sites user input.

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,14 @@ var findReplace = function(word) {
   // var inputWord = word.split(" ")
   return word
 }
+
+$(document).ready(function() {
+  $("form#find-replace").submit(function(event) {
+    var word = ($("input#input").val());
+    var result = findReplace(word);
+
+    $(".sentence").text(result);
+    $("#result").text(result);
+    event.preventDefault();
+  });
+});
